11f8273abe097b160e1281899bc486356856775b
Your Name <you@example.com>
2023-08-15 14:40:17 +0530

    Again Upgraded to Spring Boot v3.1.2, Solving Security Configuration.

--- src/main/java/com/vaistra/config/SecurityConfig.java
+++ src/main/java/com/vaistra/config/SecurityConfig.java
@@ -7,9 +7,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -29,11 +31,11 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception
     {
-        http.csrf().disable()
-                .authorizeHttpRequests().requestMatchers(Constants.PUBLIC_URLS).permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .httpBasic();
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth->auth.requestMatchers(Constants.PUBLIC_URLS).permitAll()
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults());
         return http.build();
     }
     @Bean
